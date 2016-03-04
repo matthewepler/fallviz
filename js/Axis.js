69c3a210d69605fa88fbb716481211a0f57a7c3a
Matthew Epler <matthewepler@gmail.com>
2016-03-03 19:00:41 -0500

    two scenes showing alignment of objects fixed axis plane added

--- js/Axis.js
+++ js/Axis.js
@@ -1,6 +1,6 @@
 var Axis = function(container){
 
-  var depth = 2000;
+  var depth = 200;
   // add X
   var axisGeom = new THREE.Geometry();
   var xAxisMaterial = new THREE.LineBasicMaterial({
@@ -28,5 +28,17 @@ var Axis = function(container){
   zAxis.vertices.push(new THREE.Vector3(0, 0, depth));
   var zAxisMesh = new THREE.Line(zAxis, zAxisMaterial);
   container.add(zAxisMesh);
+
+  var planeGeometry = new THREE.PlaneGeometry(200, 200);
+  var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc, opacity: 0.1});
+  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  // plane.receiveShadow = true;
+  // rotate and position the plane
+  plane.rotation.x = -0.5 * Math.PI;
+  plane.position.x = 0;
+  plane.position.y = -1;
+  plane.position.z = 0;
+  // add the plane to the scene
+  container.add(plane);
 
 };
--- js/main.js
+++ js/main.js
@@ -1,22 +1,63 @@
-var camera, gui;
+var camera, rotationCamera, gui;
 
 $(document).ready(function(){
 
-	var scene = new THREE.Scene(); 
+    /// ROTATION SCENE (UPPER-RIGHT)
+    var rotationScene = new THREE.Scene();
+    
+    rotationRenderer = new THREE.WebGLRenderer(); 
+    rotationRenderer.setClearColor(0x000000, 1.0);
+    rotationRenderer.setSize(window.innerWidth/5, window.innerWidth/5);
+    rotationRenderer.shadowMap.enabled = true; 
+    
+    rotationCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerWidth, 0.1, 1000);
+    rotationCamera.position.x = 15;
+    rotationCamera.position.y = 15;
+    rotationCamera.position.z = 13;
+    rotationCamera.lookAt(rotationScene.position);
+    
+    var planeGeometry = new THREE.PlaneGeometry(20, 20);
+    var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 0;
+    plane.position.y = -100;
+    plane.position.z = 0;
+    rotationScene.add(plane);
+    
+    var cubeGeometry = new THREE.BoxGeometry(5, 11, 1);
+    var cubeMaterial = new THREE.MeshLambertMaterial({color: 'red'});
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.castShadow = true;
+    rotationScene.add(cube);
+    
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(10, 20, 20);
+    spotLight.shadow.camera.near = 20;
+    spotLight.shadow.camera.far = 50;
+    spotLight.castShadow = true;
+    rotationScene.add(spotLight);
 
+
+    
+    /// MAIN SCENE
+    var scene = new THREE.Scene(); 
     var renderer = new THREE.WebGLRenderer({alpha:true}); 
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    var container = document.getElementById('canvas-wrap');
-    container.appendChild( renderer.domElement );
-
     camera = new THREE.PerspectiveCamera( 
-        75, window.innerWidth / window.innerHeight, 0.1, 8000);
+        45, window.innerWidth / window.innerHeight, 0.1, 8000);
     // camera.position.x = 15;
     // camera.position.y = 16;
     camera.position.z = 2000;
     camera.lookAt(scene.position);
     var cameraControl = new THREE.OrbitControls(camera, container);
+
+    // ADDING BOTH TO DOM
+    var container = document.getElementById('canvas-wrap');
+    container.appendChild( renderer.domElement );
+    container = document.getElementById('rotation-wrap');
+    container.appendChild(rotationRenderer.domElement); 
+
 
     var axis = new Axis(scene);
 
@@ -34,6 +75,9 @@ $(document).ready(function(){
     });
 
     var data = new Fall.Data();
+
+    
+
 
     // holds the resulting mesh. coming back to this. 
     var group = new THREE.Object3D(); 
@@ -69,7 +113,8 @@ $(document).ready(function(){
             if(bufferGeom) {
                 
             }
-    	renderer.render(scene, camera);
+        renderer.render( scene, camera );
+        rotationRenderer.render( rotationScene, rotationCamera );
         cameraControl.update();
     }
 
@@ -84,3 +129,18 @@ function customAlert(text) {
         $('#messages').css('opacity', 0);
     },3000);
 }
+
+
+function handleResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    rotationCamera.aspect = window.innerWidth / window.innerWidth;
+    camera.updateProjectionMatrix();
+    projectioncamera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    rotationRenderer.setSize(window.innerWidth/5, window.innerWidth/5);
+}
+
+
+window.addEventListener('resize', handleResize, false);
+$("#data-input").on('click', function(){this.select()});
+
